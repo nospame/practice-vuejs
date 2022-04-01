7ca7649a1427dc14f38b13a2bfccfd42215a9334
nospame <mapson.evan@gmail.com>
2022-04-01 10:09:44 -0700

    add initial todos js practice

---
+++ todos/todos.js
@@ -0,0 +1,31 @@
+/* global Vue */
+
+var App = {
+  data: function () {
+    return {
+      message: "Hello Vue!",
+      todos: [
+        {
+          "userId": 1,
+          "id": 1,
+          "title": "delectus aut autem",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 2,
+          "title": "quis ut nam facilis et officia qui",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 3,
+          "title": "fugiat veniam minus",
+          "completed": false
+        },
+      ]
+    };
+  }
+};
+
+Vue.createApp(App).mount('#app');
